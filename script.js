d68273418d58c36afd259af67e15998c6e7a9bdd
Benoit <benoit.exbrayat@gmail.com>
2024-04-18 15:42:44 +0200

    create a number of div into container
    add the logic of creation a new div into the container
    add a function to create and add a determined number of div into the container
    of the html page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("mqkdfmqkjdfmqz"); 
+  
+function createNewDivInContainer(){
+    const container =document.getElementById("container");
+    var newDiv = document.createElement("div"); 
+    container.appendChild(newDiv); 
+}
+
+function createNumberNewDivIntoContainer(number){
+    for(var i=0; i<number; i++){
+    createNewDivInContainer();  
+    }
+}
+
+createNumberNewDivIntoContainer(3); 
+
